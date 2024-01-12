a5c5e290291d641641405f24c4a898a8efdfe187
lllllllllllllll <louisethekidrs@gmail.com>
2024-01-12 21:30:49 +0400

    Update main.py

--- Machine Learning - Object Detection Model/main.py
+++ Machine Learning - Object Detection Model/main.py
@@ -14,26 +14,26 @@ img_preprocess = weights.transforms() #prepare the image for the model by transf
 @st.cache_resource # it is for streamlit to cache the results based on input parameters, it basically tells streamlit to store the results of the functions
 
 def load_model(): #create function load_model()
-    model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.8) #create instance of fasterRCNN together with the pre-trained weights, treshold sets the limit for considering bounding boxes predictions.
+    model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.8) #create instance of fasterRCNN together with the pre-trained weights, treshold sets the limit for considering bounding boxes predicts.
     model.eval() #tells the model to not change its training or the learned parameters or update the weights
     return model
 
 model = load_model() 
 
-#preprocess img and ready for prediction
-def make_prediction(img):#create make_prediction function with a parameter 'img'
+#preprocess img and ready for predictions
+def prediction(img):#create prediction function with a parameter 'img'
     img_processed = img_preprocess(img) #preprocess the image for the model (preparing)
-    prediction = model(img_processed.unsqueeze(0))#makes an extra batch of the preprocessed img as object detection models want images in batches, this basically prepares the preprocessed img into the model for predictions
-    prediction = prediction[0]#selects the first element from the list
-    prediction["labels"] = [categories[label] for label in prediction["labels"]] #updates the predicted labels by mapping the numerical labels obtained from the model using 'categories' mapping.
-    return prediction
+    predict = model(img_processed.unsqueeze(0))#makes an extra batch of the preprocessed img as object detection models want images in batches, this basically prepares the preprocessed img into the model for predicts
+    predict = predict[0]#selects the first element from the list
+    predict["labels"] = [categories[label] for label in predict["labels"]] #updates the predicted labels by mapping the numerical labels obtained from the model using 'categories' mapping.
+    return predict
 
-#display bounding boxes after prediction
-def create_image_with_boxes(img, prediction): #create function create_image_with_boxes with parameter img, prediction
+#display bounding boxes after predict
+def create_image_with_boxes(img, predict): #create function create_image_with_boxes with parameter img, predict
     img_tensor = torch.tensor(img)#converts the img to PyTorch tensor.
     #the actual box
-    img_with_boxes = draw_bounding_boxes(img_tensor, boxes=prediction["boxes"], labels=prediction["labels"],
-                                         colors=["red" if label == "person" else "green" for label in prediction["labels"]],
+    img_with_boxes = draw_bounding_boxes(img_tensor, boxes=predict["boxes"], labels=predict["labels"],
+                                         colors=["red" if label == "person" else "green" for label in predict["labels"]],
                                          width=4)  # Increase the width of the bounding boxes for better visibility
     create_image_with_boxes_np = img_with_boxes.detach().numpy().transpose(1, 2, 0)#extracts the img with bounding boxes from the converted PyTorch img and converts it back to Numpy array. the numbers represent the dimensions and rearrange it for img visualization purposes i.e PyTorch to standard img
     return create_image_with_boxes_np
@@ -41,9 +41,9 @@ def create_image_with_boxes(img, prediction): #create function create_image_with
 
 
 #loop through the detected bounding box and labels then convert the box coords to integers and draw bounding box
-def visualize_predictions(img, prediction):#create visualize_predictions with parameters img, prediction
+def display_predict(img, predict):#create display_predict with parameters img, predict
     img_with_boxes = np.array(img) #to input image as Numpy array (used for visualizing the bounding boxes and labels)
-    for box, label in zip(prediction["boxes"], prediction["labels"]): #loop through predictions
+    for box, label in zip(predict["boxes"], predict["labels"]): #loop through predicts
         box = [int(coord) for coord in box] #converts the 'box' to an integer
         img_with_boxes = cv2.rectangle(img_with_boxes, (box[0], box[1]), (box[2], box[3]), (255, 0, 0), 2) #uses OpenCV to draw bounding box on the image array "img_with_boxes". the rectangle box is rgb 255,0,0 and the pixel is 2
         img_with_boxes = cv2.putText(img_with_boxes, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)#displays text labels into the img array "img_with_boxes"text size is set to 0.9, in blue color (255,0,0) with a size of 2 pixel.
@@ -60,8 +60,8 @@ st.header("Upload Image Here:")
 upload = st.file_uploader(label="", type=["png", "jpg", "jpeg"])
 if upload:
     img = Image.open(upload)
-    prediction = make_prediction(img) #calls the make_prediction function with the parameter img
-    img_with_box = create_image_with_boxes(np.array(img).transpose(2, 0, 1), prediction)#calls create_image_with_boxes with various parameters in it
+    predict = prediction(img) #calls the prediction function with the parameter img
+    img_with_box = create_image_with_boxes(np.array(img).transpose(2, 0, 1), predict)#calls create_image_with_boxes with various parameters in it
     st.image(img, caption='Uploaded Image', use_column_width=True)
 
     st.write("") #empty space for layout adjustment
@@ -80,11 +80,11 @@ if upload:
         st.pyplot(fig, use_container_width=True)#tells streamlit to render plt figure, and adjust the plot to fit within streamlit
 
     with col1:
-        del prediction["boxes"]#to remove redundancy
+        del predict["boxes"]#to remove redundancy
         st.header("Predicted Probabilities")#display header 
          # Displaying Scores/Probabilities
         st.write("Labels and Probabilities:")
-        for label, score in zip(prediction["labels"], prediction["scores"].tolist()):#iterate through the labels and scores, displaying them in a more organized and readable format.
+        for label, score in zip(predict["labels"], predict["scores"].tolist()):#iterate through the labels and scores, displaying them in a more organized and readable format.
             st.write(f"- {label}: {score:.4f}")
 
 
